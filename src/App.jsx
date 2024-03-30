ccbe0ef63ba29fa5a831ec349107c9f9943ea50a
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-03-30 13:41:14 -0700

    Trying to resolve header image loading in dev but not prod

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import Info from './components/info.jsx'
 import Content from './components/content.jsx'
 import Footer from './components/footer.jsx'
+import HeaderImg from './assets/joey_beach_bw.jpg'
 
 function App() {
 
   return (
       <div className='app-wrapper'>
-        <img className='headerPortrait' src={"../src/assets/joey_beach_bw.jpg"} />
+        <img className='headerPortrait' src={HeaderImg} />
         <Info />
         <Content />
         <Footer />
