8163468da837c93a8d80ac2881510baa3df57d8d
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-04-01 12:13:37 -0700

    Made buttons functional

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,10 @@ import styles from "../components/footer.module.css"
 export default function Footer() {
     return (
         <div className={styles.footer}>
-                <button><i className="fa-brands fa-square-x-twitter fa-2xl"></i></button>
-                <button><i className="fa-brands fa-square-facebook fa-2xl"></i></button>
-                <button><i className="fa-brands fa-square-instagram fa-2xl"></i></button>
-                <button><i className="fa-brands fa-square-github fa-2xl"></i></button>
+                <button><i onClick={() => window.open('https://twitter.com/PagluicaJoey', '_blank')} className="fa-brands fa-square-x-twitter fa-2xl"></i></button>
+                <button><i onClick={() => window.open('https://www.facebook.com/joe.pagluica.9/', '_blank')} className="fa-brands fa-square-facebook fa-2xl"></i></button>
+                <button><i onClick={() => window.open('https://www.instagram.com/joeypagluica/', '_blank')} className="fa-brands fa-square-instagram fa-2xl"></i></button>
+                <button onClick={() => window.open('https://github.com/MadHapps', '_blank')}><i className="fa-brands fa-square-github fa-2xl"></i></button>
         </div>
     )
 }
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -7,11 +7,11 @@ export default function Info() {
             <h3>Frontend Developer</h3>
             <h4>joeypagluica.website</h4>
             <div className={styles.buttons}>
-                <button className={styles.email}>
+                <button className={styles.email} onClick={() => window.location.href = 'mailto:joepagluica@gmail.com'}>
                 <i className="fa-solid fa-envelope"></i>
                 Email
                 </button>
-                <button className={styles.linkedIn}>
+                <button className={styles.linkedIn} onClick={() => window.open('https://linkedin.com/in/joey-pagluica-2b774278', '_blank')}>
                 <i className="fa-brands fa-linkedin"></i>
                 LinkedIn
                 </button>
